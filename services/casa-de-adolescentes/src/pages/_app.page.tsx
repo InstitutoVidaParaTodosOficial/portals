9d5218f9118c19bb74dc00de426e245f8af1b234
Marcelo Ivo <marceloimgontijo@gmail.com>
2024-07-31 21:53:44 -0300

    feat[casa-de-adolescentes]: create topbar menu

--- services/casa-de-adolescentes/src/pages/_app.page.tsx
+++ services/casa-de-adolescentes/src/pages/_app.page.tsx
@@ -1,6 +1,12 @@
 import type { AppProps } from "next/app"
 import "../styles/global.css"
+import TopBar from "@/layout/TopBar"
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <TopBar />
+      <Component {...pageProps} />
+    </>
+  )
 }
