cf3be88bd3ace090a9bbae242893ca2ce83386ad
Marcelo Ivo <marceloimgontijo@gmail.com>
2024-07-31 21:53:44 -0300

    feat[casa-de-adolescentes]: create topbar menu

---
+++ services/casa-de-adolescentes/src/layout/TopBar.tsx
@@ -0,0 +1,77 @@
+import styled from "styled-components"
+
+import { Just_Me_Again_Down_Here } from "next/font/google"
+
+const justMeAgainDownHere = Just_Me_Again_Down_Here({
+  subsets: ["latin"],
+  weight: "400"
+})
+
+const TopBarWrapper = styled.nav`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+
+  padding: 16px 24px;
+`
+
+const TopBarNavItemsList = styled.ul`
+  display: flex;
+  gap: 46px;
+
+  text-transform: uppercase;
+  font-size: 32px;
+`
+
+const LogWrapper = styled.div`
+  width: 142px;
+  height: auto;
+`
+
+type NavItem = {
+  id: number
+  link: string
+  title: string
+}
+
+const navItems: Array<NavItem> = [
+  {
+    id: 1,
+    title: "Sobre Nós",
+    link: "#about-us"
+  },
+  {
+    id: 2,
+    title: "Casas",
+    link: "#places"
+  },
+  {
+    id: 3,
+    title: "Participar",
+    link: "#join"
+  },
+  {
+    id: 3,
+    title: '"Vem e Vê"',
+    link: "#come-and-see"
+  }
+]
+
+export default function TopBar() {
+  return (
+    <TopBarWrapper>
+      <TopBarNavItemsList className={`unstyled-list ${justMeAgainDownHere.className}`}>
+        {navItems.map(item => (
+          <li key={item.id}>
+            <a className="unstyled-anchor" href={item.link} target="_self">
+              {item.title}
+            </a>
+          </li>
+        ))}
+      </TopBarNavItemsList>
+
+      <LogWrapper></LogWrapper>
+    </TopBarWrapper>
+  )
+}
