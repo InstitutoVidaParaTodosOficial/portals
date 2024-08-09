d835eaf020b91e195ea5608815aa1a68321f6c59
Marcelo Ivo <marceloimgontijo@gmail.com>
2024-08-08 21:48:07 -0300

    feat[casa-de-adolescentes]: add events section

--- services/casa-de-adolescentes/src/pages/home/index.page.tsx
+++ services/casa-de-adolescentes/src/pages/home/index.page.tsx
@@ -1,9 +1,11 @@
 import FirstSection from "@/pages/home/sections/FirstSection"
+import NextEventsSection from "@/pages/home/sections/NextEventsSection"
 
 export default function HomePage() {
   return (
     <>
       <FirstSection />
+      <NextEventsSection />
     </>
   )
 }
