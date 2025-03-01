10956db9a54e0cec5c3c4413971453dc39db7ea6
Marcelo Ivo <marceloimgontijo@gmail.com>
2025-03-01 00:23:54 -0300

    feat[main-portal]: setup i18n in home page

--- services/main-portal/src/pages/home/index.page.tsx
+++ services/main-portal/src/pages/home/index.page.tsx
@@ -1,3 +1,14 @@
+import { useTranslations } from "next-intl"
+
 export default function HomePage() {
-  return <h1>Hello World</h1>
+  const t = useTranslations()
+  return <h1>{t("welcome")}</h1>
+}
+
+export async function getStaticProps(context) {
+  return {
+    props: {
+      messages: (await import(`./i18n/${context.locale}.json`)).default
+    }
+  }
 }
