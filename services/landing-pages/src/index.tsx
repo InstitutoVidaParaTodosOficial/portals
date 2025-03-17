36109ba52fe37c64f729e2eec7f69308bc1bba22
Marcelo Ivo <marceloimgontijo@gmail.com>
2025-03-17 16:06:55 -0300

    feat(landing-pages): setup translations

--- services/landing-pages/src/index.tsx
+++ services/landing-pages/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client"
 import { RouterProvider } from "react-router-dom"
 import { routes } from "./routes"
 import "./index.css"
+import "./i18n"
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
