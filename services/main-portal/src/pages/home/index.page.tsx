cf145551a40b26756cd15f5a9febd823faa6eba8
p3rsidh <persidhpimenta@gmail.com>
2025-07-12 20:16:52 -0300

    removing content from another branch to match main with only may i pray for you updates

--- services/main-portal/src/pages/home/index.page.tsx
+++ services/main-portal/src/pages/home/index.page.tsx
@@ -1,10 +1,8 @@
-import HeroSection from "./sections/HeroSection/HeroSection"
 import MayIPrayForYouSection from "./sections/MayIPrayForYouSection/MayIPrayForYouSection"
 
 export default function HomePage() {
   return (
     <>
-      <HeroSection />
       <MayIPrayForYouSection />
     </>
   )
