5bd2a3fae45cc421d1faa232fbb6c35be1ec1bd5
p3rsidh <persidhpimenta@gmail.com>
2025-07-22 21:51:33 -0300

    Pulling the previously existing 'title' and 'import' from main

--- services/main-portal/src/pages/home/index.page.tsx
+++ services/main-portal/src/pages/home/index.page.tsx
@@ -1,8 +1,11 @@
+import { useTranslations } from "next-intl"
 import MayIPrayForYouSection from "./sections/MayIPrayForYouSection/MayIPrayForYouSection"
 
 export default function HomePage() {
+  const t = useTranslations()
   return (
     <>
+      <h1>{t("welcome")}</h1>
       <MayIPrayForYouSection />
     </>
   )
