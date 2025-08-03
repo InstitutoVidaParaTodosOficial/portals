848062d05188db0a59376ee82694e1715266f927
Marcelo Gontijo <marceloimgontijo@gmail.com>
2025-08-03 14:21:51 -0300

    feat[main-portal]: add details section and call to action section

--- services/main-portal/src/pages/kingdom-investors/index.page.tsx
+++ services/main-portal/src/pages/kingdom-investors/index.page.tsx
@@ -1,13 +1,43 @@
 import styled from "styled-components"
 import { useTranslations } from "next-intl"
+import ReactMarkdown from "react-markdown"
 
 import HeroSection from "@/pages/kingdom-investors/sections/HeroSection/HeroSection"
 import YouTubeVideoPlayer from "@/components/youtube-video-player/YouTubeVideoPlayer"
+
+const PageWrapper = styled.div`
+  color: var(--primary-color);
+  & > * {
+    padding-left: 32px;
+    padding-right: 32px;
+  }
+`
 
 const VideoPlayerWrapper = styled.div`
   display: flex;
   justify-content: center;
   margin-top: -64px;
+`
+
+const CallToActionSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  padding: 0 72px;
+`
+
+const CallToActionButton = styled.button`
+  padding: 8px 16px;
+  color: var(--white);
+  background-color: var(--primary-color);
+  border: none;
+  p {
+    margin: 0;
+  }
+  &:hover {
+    cursor: pointer;
+  }
 `
 
 export default function IndexPage() {
@@ -16,12 +46,27 @@ export default function IndexPage() {
   const videoId = t("videos.introduction")
 
   return (
-    <>
+    <PageWrapper>
       <HeroSection />
 
       <VideoPlayerWrapper>
         <YouTubeVideoPlayer videoId={videoId} />
       </VideoPlayerWrapper>
-    </>
+
+      <section>
+        <ReactMarkdown>{t("details")}</ReactMarkdown>
+      </section>
+
+      <div>
+        <hr />
+      </div>
+
+      <CallToActionSection>
+        <h2 style={{ textAlign: "center" }}>{t("call_to_action.title")}</h2>
+        <CallToActionButton type="button">
+          <ReactMarkdown>{t("call_to_action.button")}</ReactMarkdown>
+        </CallToActionButton>
+      </CallToActionSection>
+    </PageWrapper>
   )
 }
