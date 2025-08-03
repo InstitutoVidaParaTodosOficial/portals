9d7e5e169b215b484d0a030473984ded2c4b0140
Marcelo Gontijo <marceloimgontijo@gmail.com>
2025-08-03 14:05:54 -0300

    feat[main-portal]: add first video section

--- services/main-portal/src/pages/kingdom-investors/index.page.tsx
+++ services/main-portal/src/pages/kingdom-investors/index.page.tsx
@@ -1,9 +1,27 @@
+import styled from "styled-components"
+import { useTranslations } from "next-intl"
+
 import HeroSection from "@/pages/kingdom-investors/sections/HeroSection/HeroSection"
+import YouTubeVideoPlayer from "@/components/youtube-video-player/YouTubeVideoPlayer"
+
+const VideoPlayerWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: -64px;
+`
 
 export default function IndexPage() {
+  const t = useTranslations("kingdom-investors")
+
+  const videoId = t("videos.introduction")
+
   return (
     <>
       <HeroSection />
+
+      <VideoPlayerWrapper>
+        <YouTubeVideoPlayer videoId={videoId} />
+      </VideoPlayerWrapper>
     </>
   )
 }
