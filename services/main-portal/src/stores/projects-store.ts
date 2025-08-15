7dd40ee75499c3accaad4450b48e3f38b911342d
Marcelo Gontijo <marceloimgontijo@gmail.com>
2025-08-15 14:32:46 -0300

    feat[main-portal]: add projects logo

--- services/main-portal/src/stores/projects-store.ts
+++ services/main-portal/src/stores/projects-store.ts
@@ -5,7 +5,10 @@ import MOCK_DATA from "./__mock__/projects.json"
 export type Project = {
   title: string
   order?: number
-  titleIcon?: string
+  logo?: {
+    url?: string
+    backgroundColor?: string
+  }
   paragraphs: string[]
   featuredImageUrl?: string
   imagesUrls?: string[]
